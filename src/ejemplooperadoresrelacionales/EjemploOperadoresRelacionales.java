e31b3f97ac3739fc0081204c45d4544fde6950aa
JKGzenna <juanlu360@gmail.com>
2017-06-12 00:41:25 +0200

    Ejercicio Operaciones

---
+++ src/ejemplooperadoresrelacionales/EjemploOperadoresRelacionales.java
@@ -0,0 +1,23 @@
+package ejemplooperadoresrelacionales;
+
+/**
+ *
+ * @author JUAN
+ */
+public class EjemploOperadoresRelacionales {
+
+    public static void main(String[] args) {
+        
+        int a = 6;
+        int b = 2;
+      //int b = 0;
+        
+        if(a > 3 && !(b < 1)){
+            System.out.println("Verdadero");
+        }
+        else
+        {
+        System.out.println("Falso");
+        }
+    }
+}
